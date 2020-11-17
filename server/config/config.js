329d263a4c5177d92fa0232aaa00e3be238c02cd
ronalucard <rona-lucard@outlook.com>
2020-11-17 01:43:05 -0600

    variable de entorno mongo

--- server/config/config.js
+++ server/config/config.js
@@ -12,7 +12,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe'
 } else {
-    urlDB = 'mongodb+srv://ronaldohl:I8S1WU34S9NJwZ1p@cluster0.14ckf.mongodb.net/cafe'
+    urlDB = process.env.MONGO_URI;
 }
 
 
